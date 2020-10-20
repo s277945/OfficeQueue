223ffbad2337809c6ec818f4a186b53c60eadf08
Hilderic <hilderic.bourin@gmail.com>
2020-10-20 14:35:05 +0200

    material ui/axios setup for ticket page

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import { Button, Box, ButtonGroup } from '@material-ui/core';
+import axios from 'axios';
 
 export class Ticket extends Component {
 
+    // dirty setup to try axios
+    requestType1Test(){
+        axios.post(`https://30001/api/tickets/newTicket`, { requestType : "requestType1" })
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+    }
+
     render() {
         return (
-            <div>
-                Ticket
-            </div>
+            <Box>
+                <ButtonGroup size="large" variant="contained" color="primary" aria-label="large outlined primary button group">
+                    <Button onClick={this.requestType1Test}>Request_Type1</Button>
+                    <Button>Request_Type2</Button>
+                    <Button>Request_Type3</Button>
+                </ButtonGroup>
+            </Box>
         )
     }
 }
