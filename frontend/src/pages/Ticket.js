e6f88c725333d60fc78d209e06795aae834243c6
Hilderic <hilderic.bourin@gmail.com>
2020-10-21 10:19:38 +0200

    ticket handling

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -3,24 +3,49 @@ import { Button, Box, ButtonGroup } from '@material-ui/core';
 import axios from 'axios';
 
 export class Ticket extends Component {
+    state = {
+        request_types: [],
+        ticket: null
+    }
+
+    componentDidMount(){
+        axios.get(`https://5f8ef6f0693e730016d7ab61.mockapi.io/RequestType`)
+        .then(res => {
+          this.setState({request_types: res.data})
+        })
+    }
 
     // dirty setup to try axios
-    requestType1Test(){
-        axios.post(`https://30001/api/tickets/newTicket`, { requestType : "requestType1" })
+    onRequestTypeClick(request_type){
+        axios.post(`https://localhost:3001/api/tickets`, {requestType : request_type})
         .then(res => {
-          console.log(res);
-          console.log(res.data);
+          this.setState({ticket: res.data})
         })
     }
 
     render() {
+        if(!this.state.ticket){
+            return this.renderRequestList()
+        }
+        else return this.renderTicket();
+    }
+
+    renderRequestList(){
         return (
-            <Box>
-                <ButtonGroup size="large" variant="contained" color="primary" aria-label="large outlined primary button group">
-                    <Button onClick={this.requestType1Test}>Request_Type1</Button>
-                    <Button>Request_Type2</Button>
-                    <Button>Request_Type3</Button>
+            <Box display="flex" mx={5} justifyContent="center" >
+                <ButtonGroup fullWidth orientation="vertical" variant="contained" size="large" color="primary" aria-label="large outlined primary button group">
+                    {this.state.request_types.map(request_type => 
+                        <Button key={request_type.request_type} onClick={() => this.onRequestTypeClick(request_type.request_type)}>{request_type.request_type}</Button>
+                    )}
                 </ButtonGroup>
+            </Box>
+        )
+    }
+
+    renderTicket(){
+        return (
+            <Box display="flex" mx={5} justifyContent="center" >
+                <p>Your ticket is : {this.state.ticket.id}</p>
             </Box>
         )
     }
