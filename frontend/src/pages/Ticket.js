daac4703d09ef495def51a060846e4c7637b9b14
Alessandro <s277945@studenti.polito.it>
2020-10-22 20:52:42 +0200

    ticket buttons fixed

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -34,13 +34,13 @@ export class Ticket extends Component {
 
     renderRequestList(){
         return (
-            <Box display="flex" mx={5} justifyContent="center" >
-                <ButtonGroup fullWidth orientation="vertical" variant="contained" size="large" color="primary" aria-label="large outlined primary button group">
-                    {this.state.request_types.map(request_type => 
-                        <Button key={request_type.RequestType} onClick={() => this.onRequestTypeClick(request_type.RequestType)}>{request_type.RequestType}</Button>
-                    )}
-                </ButtonGroup>
-            </Box>
+            <div style={{ minHeight: '950px' }}>
+                <div style={{ display: 'flex', flexGrow: '1', backgroundColor: '#282c34', minHeight: '80px', justifyContent: 'center', alignContent: 'center' }}><h1 style={{ color: '#fff' }}>Select a service</h1></div>
+                <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+                    {this.state.request_types.map(request_type =>
+                        <Button style={{margin: '77px',  minWidth: '150px', maxWidth: '150px', padding: '50px', fontSize: '20px'}} variant="contained" color="primary" key={request_type.RequestType} onClick={() => this.onRequestTypeClick(request_type.RequestType)}>{request_type.RequestType}</Button>
+                    )}</div>
+            </div>
         )
     }
 
