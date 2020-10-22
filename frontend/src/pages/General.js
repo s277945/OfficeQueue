e66295eef8a829392bd27c8f623ea6e0e37382df
Alessandro <s277945@studenti.polito.it>
2020-10-22 15:39:11 +0200

    nice colors

--- frontend/src/pages/General.js
+++ frontend/src/pages/General.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import { createMuiTheme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,19 +8,6 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-
-
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
 
 export class General extends Component {
     state = {
@@ -41,17 +29,17 @@ export class General extends Component {
             <div>
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
-                    <TableHead>
+                    <TableHead style={{backgroundColor: '#282c34'}}>
                     <TableRow>
-                        <TableCell align="right">Counter</TableCell>
-                        <TableCell align="right">RequestTypes</TableCell>
-                        <TableCell align="right">TicketNumber</TableCell>
+                        <TableCell style={{color: '#fff'}} align="right">Counter</TableCell>
+                        <TableCell style={{color: '#fff'}} align="right">RequestTypes</TableCell>
+                        <TableCell style={{color: '#fff'}} align="right">TicketNumber</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
                     {this.state.data.map(elem => 
                         <TableRow key={elem[0].counterId}>
-                        <TableCell component="th" scope="row">
+                        <TableCell align="right" component="th" scope="row">
                             {elem[0].counterId}
                         </TableCell>
                         <TableCell align="right">{elem[0].requestType}</TableCell>
